530ef8296d079df2cc65d849421e5a56633ee113
TAKAHARA <mwl9dv@gmail.com>
2020-08-30 13:02:53 +0900

    MySGP30クラスのインスタンスでサンプル処理置き換え

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,59 @@
 #include <Adafruit_SGP30.h>
 #include <Arduino.h>
+#include <MySGP30.h>
 #include <Wire.h>
 
-Adafruit_SGP30 sgp;
-
-/* return absolute humidity [mg/m^3] with approximation formula
- * @param temperature [°C]
- * @param humidity [%RH]
- */
-uint32_t getAbsoluteHumidity(float temperature, float humidity) {
-    // approximation formula from Sensirion SGP30 Driver Integration
-    // chapter 3.15
-    const float absoluteHumidity =
-        216.7f * ((humidity / 100.0f) * 6.112f *
-                  exp((17.62f * temperature) / (243.12f + temperature)) /
-                  (273.15f + temperature)); // [g/m^3]
-    const uint32_t absoluteHumidityScaled =
-        static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
-    return absoluteHumidityScaled;
-}
+MySGP30 sgp;
 
 void setup() {
     Serial.begin(115200);
+    delay(5000);
     Serial.println("SGP30 test");
 
-    if (!sgp.begin()) {
+    while (!sgp.isEnabled()) {
         Serial.println("Sensor not found :(");
-        while (1)
-            ;
     }
-    Serial.print("Found SGP30 serial #");
-    Serial.print(sgp.serialnumber[0], HEX);
-    Serial.print(sgp.serialnumber[1], HEX);
-    Serial.println(sgp.serialnumber[2], HEX);
 
-    // If you have a baseline measurement from before you can assign it to
-    // start, to 'self-calibrate' sgp.setIAQBaseline(0x8E68, 0x8F41);  // Will
-    // vary for each sensor!
+    Serial.print("Found SGP30 serial: ");
+    Serial.println(sgp.getSerial());
+
+    /*
+    If you have a baseline measurement from before you can assign it to start,
+    to 'self-calibrate' sgp.setIAQBaseline(0x8E68, 0x8F41); Will vary for each
+    sensor!
+    */
 }
 
 int counter = 0;
 void loop() {
-    // If you have a temperature / humidity sensor, you can set the absolute
-    // humidity to enable the humditiy compensation for the air quality signals
-    // float temperature = 22.1; // [°C]
-    // float humidity = 45.2; // [%RH]
-    // sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
+    /*
+    If you have a temperature / humidity sensor, you can set the absolute
+    humidity to enable the humditiy compensation for the air quality signals.
+    float temperature = 22.1; // [°C]
+    float humidity = 45.2; // [%RH]
+    sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
+    */
 
-    if (!sgp.IAQmeasure()) {
+    if (!sgp.canMeasureIAQ()) {
         Serial.println("Measurement failed");
         return;
     }
     Serial.print("TVOC ");
-    Serial.print(sgp.TVOC);
+    Serial.print(sgp.getTVOC());
     Serial.print(" ppb\t");
     Serial.print("eCO2 ");
-    Serial.print(sgp.eCO2);
+    Serial.print(sgp.getECO2());
     Serial.println(" ppm");
 
-    if (!sgp.IAQmeasureRaw()) {
+    if (!sgp.canMeasureRawIAQ()) {
         Serial.println("Raw Measurement failed");
         return;
     }
     Serial.print("Raw H2 ");
-    Serial.print(sgp.rawH2);
+    Serial.print(sgp.getRawH2());
     Serial.print(" \t");
     Serial.print("Raw Ethanol ");
-    Serial.print(sgp.rawEthanol);
+    Serial.print(sgp.getRawEthanol());
     Serial.println("");
 
     delay(1000);
@@ -75,14 +62,13 @@ void loop() {
     if (counter == 30) {
         counter = 0;
 
-        uint16_t TVOC_base, eCO2_base;
-        if (!sgp.getIAQBaseline(&eCO2_base, &TVOC_base)) {
+        if (!sgp.canGetIAQBaseLine()) {
             Serial.println("Failed to get baseline readings");
             return;
         }
         Serial.print("****Baseline values: eCO2: 0x");
-        Serial.print(eCO2_base, HEX);
+        Serial.print(sgp.getECO2Base(), HEX);
         Serial.print(" & TVOC: 0x");
-        Serial.println(TVOC_base, HEX);
+        Serial.println(sgp.getTVOCBase(), HEX);
     }
 }
